393a2103afd313c26b10e25ee946f1e3e8d0a2a2
Gabriel R Pacheco <gabriel.pacheco@brino.cc>
2020-04-15 12:45:36 -0300

    TIrando parte do código de exemplo

--- Brino.cpp
+++ Brino.cpp
@@ -8,19 +8,6 @@
 #include "Arduino.h"
 #include "Brino.h"
 
-blk_n::blk_n(int pin){
-pinMode(pin, OUTPUT);
-_pin = pin;
-
-}
-
-void blk_n::blk_led(int ms){
-  digitalWrite(_pin, HIGH);
-  delay(ms);
-  digitalWrite(_pin, LOW);
-  delay(ms);
-}
-
 // Pino 
 void ligar(int porta){
 	digitalWrite(porta, HIGH);
